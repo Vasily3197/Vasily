b7587e2857662a19871e1ed69924bd89b2785f2d
Vasily Green <vasilygreen97@gmail.com>
2023-05-22 15:22:50 +0400

    Исправил задачу 2

--- task 02/Program.cs
+++ task 02/Program.cs
@@ -6,18 +6,23 @@
 // a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Здравствуйте! Что бы определить, какое из двух чисел большее, а какое меньшее, введите, пожалуйста число a");
-double a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Теперь введите число b");
-double b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
-double max = a;
-double min = b;
+int max = a;
+int min = b;
 
-if (a>max) max = a;
+if (a>b) max = a;
 else
 {
     max = b;
+}
+
+if (a>b) min = b;
+else
+{
     min = a;
 }
 
